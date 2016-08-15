d9a877f7e13dcaf1152add6dedb95c6f0849cab6
Nataniel Strack <nataniel.strack@gmail.com>
2016-08-15 19:29:32 -0300

    Functional_test refactoring, reusing find_in_row

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_later(self):
         # Edith has heard about a cool new online to-do app, so she goes to check out its homepage 
         self.browser.get('http://localhost:8000')
@@ -35,10 +41,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock fethers to make a fly" (Edity is very methodical)
@@ -48,9 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys('Use peacock fethers to make a fly')
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Use peacock fethers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock fethers to make a fly')
 
         # The page updates again, and now shows both itens on her list
         self.fail('Finish the test!')
