8a17927a812cef0d6582dbe7b62ccbc43783c513
Nataniel Strack <nataniel.strack@gmail.com>
2016-08-20 23:07:32 -0300

    Save list refactoring to new url

--- lists/tests.py
+++ lists/tests.py
@@ -25,29 +25,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-
 
 class ItemModelTest(TestCase):
 
@@ -69,12 +46,6 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
 
 
-    def test_home_page_only_save_items_when_necessary(self):
-        request = HttpRequest()
-        response = home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
-
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
@@ -90,3 +61,18 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text':'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text':'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
--- lists/views.py
+++ lists/views.py
@@ -5,14 +5,17 @@ from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-    if request.method == 'POST':    
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, 'home.html')
 
 
 def view_lists(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items':items})
+
+
+def new_list(request):
+    if request.method == 'POST':    
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/lists/the-only-list-in-the-world/')
+
