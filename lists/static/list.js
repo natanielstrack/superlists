ce8935d56cd7d0c12e153134a6a78f9424ecc4f8
Nataniel Strack <nataniel.strack@gmail.com>
2016-09-15 21:23:13 -0300

    Adjustments in the javascript initialization

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,3 +1,10 @@
-$('input').keypress( function(){
-    $('.has-error').hide();
+function init () {
+    $('input').keypress( function(){
+        $('.has-error').hide();
+    });
+}
+
+
+jQuery(document).ready(function ($) {
+    init();
 });
